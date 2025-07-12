07b20a1fc2e166e03681752e375211d62891c968
hamrayev7 <samandarhamrayev555@gmail.com>
2025-07-12 14:38:11 +0500

    fix staticfiles_dirs

--- jobfinder/settings.py
+++ jobfinder/settings.py
@@ -90,6 +90,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
